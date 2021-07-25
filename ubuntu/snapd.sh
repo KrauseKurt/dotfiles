42854b571b3103e02e06581eb6c6658495b72363
Kurt Krause <jurgen.krause@gmail.com>
2021-07-25 04:26:57 -0300

    Things were tested

---
+++ ubuntu/snapd.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo snap install code --classic
+#sudo snap install intellij-idea-ultimate --classic
+#sudo snap install pycharm-professional --classic
+#sudo snap install webstorm --classic
+#sudo snap install datagrip --classic
+#sudo snap install rider --classic
+sudo snap install spotify
